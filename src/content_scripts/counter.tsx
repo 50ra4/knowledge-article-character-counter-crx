7d241c5c3222e955d27778ec737b18629b4a82d8
50ra4 <50ra4.dev.dev@gmail.com>
2022-08-28 19:55:30 +0900

    feat: show character count

--- src/content_scripts/counter.tsx
+++ src/content_scripts/counter.tsx
@@ -1,25 +1,42 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+type TagName = keyof HTMLElementTagNameMap;
+
+const getContentElement = (): HTMLElement | null =>
+  document.querySelector('div#content');
+
+const CODE_TAGS: TagName[] = ['pre'];
+
+const countArticle = (excludedTags: TagName[]) =>
+  (Array.from(getContentElement()?.children ?? []) as HTMLElement[])
+    .map((elm) => ({
+      tagName: elm.tagName.toLowerCase() as TagName,
+      text: elm.innerText,
+    }))
+    .filter(({ tagName }) => !excludedTags.includes(tagName)) // 特定のタグを除く
+    .reduce((acc, cur) => acc + cur.text.length, 0);
+
 const Root = () => {
+  const count = countArticle(CODE_TAGS);
+
   return (
     <>
-      <h1>Counter Sample</h1>
+      <h1>字数：{count}文字</h1>
     </>
   );
 };
 
 const render = () => {
   const root = document.createElement('div');
-  const contentElement: HTMLElement | null =
-    document.querySelector('div#content');
+  const contentElement = getContentElement();
 
   if (!contentElement) {
     console.warn('not found knowledge content!');
     return;
   }
 
-  contentElement.prepend(root);
+  contentElement.parentElement?.prepend(root);
   createRoot(root).render(
     <StrictMode>
       <Root />
