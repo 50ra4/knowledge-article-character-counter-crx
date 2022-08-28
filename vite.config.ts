4e265da055a04fcf34913c28af6883eebcb0c160
50ra4 <48591598+50ra4@users.noreply.github.com>
2022-08-28 17:37:50 +0900

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { crx } from '@crxjs/vite-plugin';
+
+import manifest from './manifest.config';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, ...config }) => ({
+  ...config,
+  build: {
+    outDir: command === 'build' ? 'extension' : 'dist',
+  },
+  plugins: [react(), crx({ manifest })],
+}));
