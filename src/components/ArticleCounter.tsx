4bbee1c1d3405c7a8305e8810edfd67194aaaa6f
50ra4 <50ra4.dev.dev@gmail.com>
2022-09-03 18:18:30 +0900

    change: ArticleCounter style

--- src/components/ArticleCounter.tsx
+++ src/components/ArticleCounter.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 export const ArticleCounter = ({ count }: { count: number }) => {
   return (
-    <div>
-      <strong>{`記事の字数: ${count}文字`}</strong>
+    <div
+      style={{
+        border: '1px solid #2c3e50',
+        backgroundColor: '#2c3e50',
+        display: 'flex',
+        flex: '1 0 auto',
+        justifyContent: 'center',
+        color: '#ffffff',
+        fontSize: '24px',
+        padding: '8px 0',
+      }}
+    >
+      記事の字数:
+      <strong style={{ fontWeight: 'bold', margin: '0 4px' }}>{count}</strong>
+      文字
     </div>
   );
 };
