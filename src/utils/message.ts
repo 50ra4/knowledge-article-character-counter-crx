829e084593d2f0ebba149b23fe019aefb85f2333
50ra4 <50ra4.dev.dev@gmail.com>
2022-09-06 23:39:20 +0900

    add: message.ts

---
+++ src/utils/message.ts
@@ -0,0 +1,22 @@
+import { FetchDraftArticleResponse } from '../types';
+
+export type MessageResponse<T, E extends Error = Error> =
+  | { type: 'success'; data: T }
+  | { type: 'error'; error: E };
+
+export type MessageState<T, E extends Error = Error> =
+  | MessageResponse<T, E>
+  | { type: 'pending' }
+  | { type: 'loading' };
+
+export type SendMessageRequestKey = 'FETCH_DRAFT_ARTICLE_COUNT';
+
+export type SendMessageRequest = {
+  type: 'FETCH_DRAFT_ARTICLE_COUNT';
+  payload: unknown;
+  return: MessageResponse<FetchDraftArticleResponse>;
+};
+
+export type SendMessageResponse = {
+  FETCH_DRAFT_ARTICLE_COUNT: MessageResponse<FetchDraftArticleResponse>;
+};
