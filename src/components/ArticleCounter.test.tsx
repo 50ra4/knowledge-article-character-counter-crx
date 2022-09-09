3fe84c903c9a7401d3ad4b25b6012fd65840fbab
50ra4 <50ra4.dev.dev@gmail.com>
2022-09-09 20:30:23 +0900

    fix: ArticleCounter failed test

--- src/components/ArticleCounter.test.tsx
+++ src/components/ArticleCounter.test.tsx
@@ -5,7 +5,7 @@ import { ArticleCounter } from './ArticleCounter';
 
 describe('ArticleCounter', () => {
   test('render component', () => {
-    render(<ArticleCounter count={3} />);
-    expect(screen.getByText(`記事の字数: 3文字`)).toBeInTheDocument();
+    render(<ArticleCounter count={7543} />);
+    expect(screen.getByText(7543)).toBeInTheDocument();
   });
 });
